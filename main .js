2f6ba25b98991aa904ab41ada12a87f47b6caf3f
Ayaanshah111 <89080721+Ayaanshah111@users.noreply.github.com>
2022-07-07 15:37:01 +0530

    Add files via upload

---
+++ main .js
@@ -0,0 +1,14 @@
+Webcam.set({
+    width:350,height:300,image_format:'png',png_quality:90
+});
+camera=document.getElementById("camera");
+Webcam.attach('#camera');
+function snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img   id="caputred_image"src="'+data_uri+'"/>';
+    });
+}
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/LHXRn6QNK/model.json',modelloaded);
+function modelloaded(){
+    console.log('model loaded');
+}
